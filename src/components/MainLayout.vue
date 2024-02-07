11fa2a3a8f7b8924bd58a8133c3e59b033f5aa74
brvnfr <ymmk93@gmail.com>
2024-02-07 00:55:34 -0300

    [wip]

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,43 +1,64 @@
+<!-- src/components/AppLayout.vue -->
+
 <template>
-  <div>
-    <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/dashboard">Dashboard</RouterLink>
-        <RouterLink to="/tasks">Tarefas</RouterLink>
-        <RouterLink to="/settings">Configurações</RouterLink>
-      </nav>
-    </header>
-    <main>
-      <router-view></router-view>
-    </main>
+  <div class="app-layout">
+    <nav class="vertical-nav">
+      <!-- Adicione links de navegação aqui -->
+      <router-link to="/">Home</router-link>
+      <router-link to="/dashboard">Dashboard</router-link>
+      <router-link to="/tasks">Tarefas</router-link>
+      <router-link to="/settings">Configurações</router-link>
+    </nav>
+    <div class="content-wrapper">
+      <header>
+        <!-- Conteúdo do cabeçalho aqui -->
+      </header>
+      <main>
+        <router-view></router-view>
+      </main>
+      <footer>
+        <!-- Conteúdo do rodapé aqui -->
+      </footer>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Layout',
-}
-</script>
-
 <style lang="stylus" scoped>
-/* Adicione estilos do layout aqui conforme necessário */
-header {
-  background-color: #3498db;
-  padding: 10px;
-  text-align: center;
 
-  nav {
-    a {
-      color: #fff;
-      margin: 0 10px;
-      text-decoration: none;
-      font-weight: bold;
-    }
+@import '../styles/variables.styl';
+.app-layout {
+  display: flex;
+}
+
+.vertical-nav {
+  background: brand-blue-400;
+  color: #fff;
+  padding: 20px;
+  width: 200px;
+  position: fixed;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    margin-bottom: 10px;
   }
 }
 
-main {
+.content-wrapper {
+  margin-left: 200px;
+  padding: 20px;
+  flex: 1;
+
+  header, main, footer {
+    margin-bottom: 20px;
+  }
+}
+
+header, main, footer {
+  background-color: brand-gray-100;
   padding: 20px;
 }
 </style>
