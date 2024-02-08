52c76ec2ea9e3e964556bb1f357667e59900a6f9
brnfr <ymmk93@gmail.com>
2024-02-08 15:06:39 -0300

    [fix] emit problem on formwrapper component

--- src/components/FormWrapper.vue
+++ src/components/FormWrapper.vue
@@ -1,20 +1,16 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <slot></slot>
-    <button-component :type="buttonType" @click="handleButtonClick" type="button">{{
-      buttonText
-    }}</button-component>
   </form>
 </template>
 
 <script setup>
-const { emit } = defineProps(['buttonText', 'buttonType'])
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps(['buttonText', 'buttonType'])
+const emit = defineEmits()
 
 const handleSubmit = () => {
   emit('submit')
-}
-
-const handleButtonClick = () => {
-  emit('buttonClick')
 }
 </script>
