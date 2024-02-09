6829e0c2a9d0d39836f468b4d7966c2eaa1b241d
brvnfr <ymmk93@gmail.com>
2024-02-09 00:18:56 -0300

    [wip] edit button style

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -19,4 +19,19 @@ const handleClick = () => {
 }
 </script>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+@import '.././styles/variables.styl'
+  
+button
+  width 100%
+  background-color brand-green-500
+  border-radius 5px
+  color white
+  border none
+  padding 1.25rem 2rem
+  cursor pointer
+  transition  all .5s ease
+  text-styles(16px, bold, #fff, 1)
+  &:hover
+    background darken(brand-green-500, 10%)
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -30,7 +30,7 @@
               autocomplete="current-password"
             />
           </div>
-          <button-component type="submit" class="login-btn"> Login </button-component>
+          <button-component type="submit"> Entrar </button-component>
         </FormWrapper>
       </main>
     </section>
@@ -141,14 +141,4 @@ label
   font-size 1rem
   border 1px solid #ccc
   border-radius 4px
-
-.login-btn
-  background brand-blue-500
-  color #fff
-  padding 0.5rem 1rem
-  border none
-  border-radius 4px
-  cursor pointer
-  &:hover
-    background darken(brand-blue-500, 10%)
 </style>
