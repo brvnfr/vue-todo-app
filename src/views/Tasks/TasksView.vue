f22f4db4f0c6a61ee6a43b98e04e27734614e8e4
brvnfr <ymmk93@gmail.com>
2024-02-11 12:06:06 -0300

    tasklist component

--- src/views/Tasks/TasksView.vue
+++ src/views/Tasks/TasksView.vue
@@ -23,40 +23,14 @@
       </div>
       <!-- Lista de Tarefas -->
       <ul class="task-list">
-        <li v-for="(task, index) in tasks" :key="index" class="task-card">
-          <div class="task-title">
-            <div class="task-check">
-              <input
-                v-model="task.completed"
-                type="checkbox"
-                id="task-checkbox"
-                :value="task.completed"
-              />
-            </div>
-            <div class="task-description">
-              {{ task.title }}
-            </div>
-          </div>
-          <div class="category-badge">
-            <span>{{ task.category }}</span>
-            <div class="dropdown" ref="dropdownRef" @click="toggleDropdown(index)">
-              <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" />
-              <div v-if="showDropdown === index" class="dropdown-menu">
-                <font-awesome-icon
-                  icon="fa-solid fa-ellipsis-vertical"
-                  class="dropdown-icon"
-                  @click="toggleDropdown(index)"
-                />
-                <button class="dropdown-button" @click="editTaskDialog(index)">
-                  <span class="dot"></span>Editar
-                </button>
-                <button class="dropdown-button" @click="deleteTask(index)">
-                  <span class="dot"></span>Excluir
-                </button>
-              </div>
-            </div>
-          </div>
-        </li>
+        <task-card
+          v-for="(task, index) in tasks"
+          :key="index"
+          :task="task"
+          :index="index"
+          @editTask="openEditTaskDialog"
+          @deleteTask="deleteTask"
+        />
       </ul>
 
       <!-- Diálogo para adicionar tarefa -->
@@ -113,6 +87,7 @@ import FormWrapper from '@/components/Form/FormWrapper.vue'
 import TextAreaComponent from '@/components//Form/Inputs/TextAreaComponent.vue'
 import RadioListComponent from '@/components/Form/Inputs/RadioListComponent.vue'
 import InputComponent from '@/components/Form/Inputs/InputComponent.vue'
+import TaskCard from '@/views/Tasks/components/TaskCard.vue'
 
 const store = useStore()
 
@@ -260,109 +235,6 @@ onMounted(() => {
   display inline-flex
   align-items center
 
-.task-card
-  padding 2rem 1rem
-  margin 0.5rem 0
-  border-radius 5px
-  display flex
-  align-items center
-  justify-content: space-between
-
-  .task-description
-    max-width 150px
-
-  .dropdown
-    position relative
-    cursor pointer
-
-    .dropdown-menu
-      position absolute
-      padding 1rem
-      top -15px
-      right -16px
-      width 109px
-      background-color white
-      box-shadow 0px 2px 4px rgba(0, 0, 8, 0.3)
-      border-radius 5px
-      padding 8px
-      z-index 2
-
-      .dropdown-button
-        display block
-        padding .5rem 1rem
-        margin-left 8px
-        text-align center
-        border none
-        background none
-        cursor pointer
-        position relative
-        overflow hidden
-        text-styles(14px, 600, brand-gray-800, 1)
-
-        &::before
-          content ""
-          width 9px
-          height 9px
-          background-color brand-gray-500
-          border-radius 50%
-          display inline-block
-          position absolute
-          left 0
-          top 50%
-          transform translateY(-50%)
-          transition background-color 0.3s ease
-
-        &:hover
-          &::before
-            background-color brand-green-500
-
-      .dropdown-icon
-        margin 8px
-        position absolute
-        top 8px
-        right 8px
-        color brand-blue-500
-        cursor pointer
-
-  .task-check
-    height 32px
-    width 32px
-
-    input[type=checkbox]
-      position relative
-      border 2px solid white
-      border-radius 5px
-      background brand-gray-200
-      cursor pointer
-      line-height 0
-      margin 0 .6em 0 0
-      outline 0
-      padding 0 !important
-      vertical-align text-top
-      height 32px
-      width 32px
-      -webkit-appearance none
-
-      &:hover
-        opacity 1
-
-      &:checked
-        background-color brand-green-400
-        opacity 1
-
-      &:before
-        content ''
-        position absolute
-        right 40%
-        top 20%
-        width 6px
-        height 14px
-        border solid brand-gray-200
-        border-width 0 3px 3px 0
-        margin -1px -1px 0 -1px
-        transform rotate(45deg)
-        translate(-50%, -50%)
-        z-index 2
 
 .form-buttons
   width 100%
@@ -373,35 +245,4 @@ onMounted(() => {
 
 label
   text-styles(16px, 300, brand-gray-950, 1)
-
-div
-  gap 1rem
-
-ul
-  list-style none
-  padding 0
-
-li
-  box-shadow-mixin(0, 2px, 4px, rgba(0, 0, 0, 0.1))
-  background-color white
-  border-radius 4px
-  margin 0.5rem
-  padding 1.5rem
-  display flex
-  justify-content space-between
-  text-styles(16px, 700, brand-gray-800, 1)
-  text-align start
-
-.task-description
-  width -webkit-fill-available
-  max-width 250px
-  overflow hidden
-  text-overflow ellipsis
-  text-align start
-
-.task-header
-  display flex
-  justify-content space-between
-  align-items center
-  width 80%
 </style>
