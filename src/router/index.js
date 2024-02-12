ddb906fc72daf56ba05dab3579d7ddfda1b161f8
brvnfr <ymmk93@gmail.com>
2024-02-12 00:50:54 -0300

    usde webhash instead history on route

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import store from '@/store'
 
 const isAuthenticated = () => {
@@ -8,7 +8,7 @@ const isAuthenticated = () => {
 const routes = [
   {
     path: '/',
-    redirect: '/login',
+    redirect: '/tasks',
     component: () => import('@/components/Layout/MainLayout.vue'),
     meta: { requiresAuth: true },
     children: [
@@ -40,15 +40,15 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(), // Alterado para usar createWebHashHistory
   routes,
 })
 
-//~ guarda de autenticação
+// // Guarda de autenticação
 // router.beforeEach((to, from, next) => {
-//   //~ Verifica se a rota requer autenticação
+//   // Verifica se a rota requer autenticação
 //   if (to.matched.some((record) => record.meta.requiresAuth)) {
-//     //~ Redireciona para a página de login se não estiver autenticado
+//     // Redireciona para a página de login se não estiver autenticado
 //     if (!isAuthenticated()) {
 //       next('/login')
 //     } else {
